afc8989476deab9c4d6f2e0fe00606a09c04e856
Nguyen Minh Nhat <3=2051052092nhat@ou.edu.vn>
2023-02-18 13:00:07 +0700

    FIRST

---
+++ AnToanHeThongThongTin/UngDungGiaoDich/UngDungGiaoDich/Controllers/AppController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UngDungGiaoDich.Controllers
+{
+    public class AppController : Controller
+    {
+  
+        // GET: App
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult ChungKhoan()
+        {
+            return View();
+        }
+        public ActionResult Vang()
+        {
+            return View();
+        }
+        public ActionResult NhaDat()
+        {
+            return View();
+        }
+        public ActionResult Login() {
+            return View();
+        }
+        public ActionResult SignIn() {
+            return View();
+        }
+    }
+}
