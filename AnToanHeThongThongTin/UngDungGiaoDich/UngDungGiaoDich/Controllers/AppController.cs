121a3b37825ae33d6ee6cd3553868f6a3a00570e
Nguyen Minh Nhat <3=2051052092nhat@ou.edu.vn>
2023-02-18 15:59:20 +0700

    Aaaa

--- AnToanHeThongThongTin/UngDungGiaoDich/UngDungGiaoDich/Controllers/AppController.cs
+++ AnToanHeThongThongTin/UngDungGiaoDich/UngDungGiaoDich/Controllers/AppController.cs
@@ -14,7 +14,6 @@ namespace UngDungGiaoDich.Controllers
         // GET: App
         public ActionResult Index()
         {
-            
             return View();
         }
         public ActionResult ChungKhoan()
